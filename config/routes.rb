eaed66ec089ecf69e296154b2276d1b94483c3fd
witoldzol <blue3656@gmail.com>
2018-03-31 19:05:16 +0200

    added MVC for about and contact sites

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'items#index'
+  
+  get 'site/about' 
+
+  get 'site/contact'
+
+  get '/about' => 'site#about'
+
+  get '/contact' => 'site#contact'
+
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
